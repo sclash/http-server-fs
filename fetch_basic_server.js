9606e4c34dabadc30b9180198d4fd03da3f9cb51
sclash <andrea.sergi1@gmail.com>
2023-07-17 09:02:03 +0200

    gitignore

---
+++ fetch_basic_server.js
@@ -0,0 +1,11 @@
+fetch("http://172.26.166.80:8000/Documents/", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9,it;q=0.8",
+    "upgrade-insecure-requests": "1",
+    "Referer": "http://172.26.166.80:8000/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": null,
+  "method": "GET"
+});
