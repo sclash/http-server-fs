d414c2dc0d97e48cace0b322db1b4753c6ffe2ff
sclash <andrea.sergi1@gmail.com>
2024-06-26 17:27:05 +0200

    handle wlan0 interface

--- find_ipv4.py
+++ find_ipv4.py
@@ -23,7 +23,7 @@ def get_wlan_ipv4_address():
             for address in interface_addresses:
                 if address.family.name == 'AF_INET':
                     ipv4_address_eth = address.address
-        if "Wi-Fi" in interface_name or 'wlp1s0' in interface_name:
+        if "Wi-Fi" in interface_name or 'wlp1s0' in interface_name or 'wlan0' in interface_name:
             for address in interface_addresses:
                 if address.family.name == "AF_INET":
                     ipv4_address = address.address
@@ -38,7 +38,8 @@ wlan_ipv4_address = get_wlan_ipv4_address()
 if wlan_ipv4_address:
     print(f"WLAN IPv4 Address: {wlan_ipv4_address}")
     # with open("/home/andreasergi/Documents/Repos/http_wlan/ip_address.txt", 'w') as f:
-    with open("/home/andreasergi/Documents/http_wlan/ip_address.txt", 'w') as f:
+    # with open("/home/andreasergi/Documents/http_wlan/ip_address.txt", 'w') as f:
+    with open("/home/andreasergi/Documents/http-server-fs/ip_address.txt", 'w') as f:
         f.write(wlan_ipv4_address)
 else:
     print("WLAN interface not found or does not have an IPv4 address.")
--- serve.sh
+++ serve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-python3 /home/andreasergi/Documents/http_wlan/find_ipv4.py
-value=$(</home/andreasergi/Documents/http_wlan/ip_address.txt)  
+python3 /home/andreasergi/Documents/http-server-fs/find_ipv4.py
+value=$(</home/andreasergi/Documents/http-server-fs/ip_address.txt)  
 echo "$value"  
 python3 -m http.server -b $value
